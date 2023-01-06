39c2c098977d9a223ad752e7f99630963f02c2cd
Tomás Sousa <118855300+TommySousa@users.noreply.github>
2023-01-06 16:22:39 +0000

    Added the basic logic that adds bbooks to the page

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ const addBook = (book) => {
     return myLibrary.push(book)   
 };
 
-const theHobbit = new Book ('The hobbit', 'J.R.R Tolkien', '269', 'Read');
-const It = new Book ('It', 'Stephen King', '269', 'Read');
-const theShining = new Book ('Tomilio', 'TOmás Sousa', '269', 'Read');
-
 const showBooks = (library) => {
     const mainContent = document.querySelector('.main-content');
     library.forEach(book => {
@@ -28,10 +24,38 @@ const showBooks = (library) => {
                 <p class="author">Author: ${book.author}</p>
                 <p class="pages">Pages Number: ${book.pages} </p>
                 <p class="read">Read: ${book.read} </p>
+                <button> Mark Read </button>
+                <button> Delete </button>
         `  
     mainContent.appendChild(element)
     });
 };
+
+const button = document.getElementById('add'); 
+const form = document.getElementById('form')
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    console.log(e.target[0].name);
+
+    const mainContent = document.querySelector('.main-content');
+    let element = document.createElement('div');
+    element.classList.add('book-card');
+    element.innerHTML = `
+            <p class="title">Title: ${e.target[0].name} </p>
+            <p class="author">Author: ${e.target[1].name}</p>
+            <p class="pages">Pages Number: ${e.target[2].name} </p>
+            <p class="read">Read: ${e.target[3].name} </p>
+            <button> Mark Read </button>
+            <button> Delete </button>
+    `  
+mainContent.appendChild(element)
+
+})
+
+
 
 const displayForm = () => {
     const formSection = document.querySelector('.add-book');
@@ -39,8 +63,10 @@ const displayForm = () => {
         formSection.style.display = 'block'
     } else if (formSection.style.display === 'block'){
         formSection.style.display = ''
-    }
-    
-}
+    } 
+};
+
+
+
 
 window.onload = (showBooks(myLibrary));
